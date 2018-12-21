e99e220b029faf216a17dac71e5615594be3bea5
Rogerio Damião <rogeriodominus@gmail.com>
2018-12-20 23:57:15 -0200

    Função Factory

---
+++ Função/factory2.js
@@ -0,0 +1,10 @@
+//Factory com Parâmetros
+function criarProduto(nome, preco){
+    return{
+        nome,
+        preco,
+        desconto: 0.1
+    }
+}
+
+console.log(criarProduto('Arroz', 15));
