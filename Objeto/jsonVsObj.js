f1a57e8eb11142c6a4850e8bc2fb47e6d5a61eca
Rogerio Damião <rogeriodominus@gmail.com>
2019-01-15 21:29:00 -0200

    Diferenças Json e Object

---
+++ Objeto/jsonVsObj.js
@@ -0,0 +1,15 @@
+const obj = { 
+        a: 1, 
+        b: 2, 
+        c: 3, 
+        soma (){
+            return a + b + c
+        }
+    };
+
+    console.log(JSON.stringify(obj));
+
+    //console.log(JSON.parse("{a: 1, b: 2, c: 3}")); Erro!
+    //console.log(JSON.parse("{'a': 1, 'b': 2, 'c': 3}")); Erro!
+    console.log(JSON.parse('{"a": 1, "b": 2, "c": 3}'));
+    
