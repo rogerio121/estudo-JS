5ff2413426fee242e9322d4284d18b2d4434dd5f
Rogerio Damião <rogeriodominus@gmail.com>
2019-02-04 22:43:49 -0200

    Introdução ao método concat

---
+++ Array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Laura', 'Sibalena']
+const filhos = ['Cristopher', 'Gabriel']
+
+const filhosEFilhas = filhas.concat(filhos)
+
+console.log(filhosEFilhas)
+console.log([].concat([1, 2], [3, 5], [[6, 7]]))
