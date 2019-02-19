2ef7847de075951b5dc009705743b0ba9c888d6f
Rogerio Damião <rogeriodominus@gmail.com>
2019-02-18 21:30:03 -0300

    This no node, dentro e fora de uma função

---
+++ Node/this.js
@@ -0,0 +1,16 @@
+//O thid dentro de uma função referencia o escopo Global
+//Mas quando fora, referencia a module.exports/exports
+
+console.log(this === global)
+console.log(this === module)
+
+console.log(this === module.exports)
+console.log(this === exports)
+
+function logThis(){
+    console.log('Dentro de uma função...')
+    console.log(this === exports)
+    console.log(this === global)
+}
+
+logThis()
