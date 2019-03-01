38bbafec13e53c982bbe341bbeb702a682a4d929
Rogerio Damião <rogeriodominus@gmail.com>
2019-03-01 00:20:25 -0300

    Get com parâmetro, Post, Put e Delete

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -2,9 +2,40 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/produtos', (req, res, next) =>{
-    res.send({nome: 'Notebook', preco: 2300.00}) //Converter para Json
+    res.send(bancoDeDados.getProdutos()) //Converter para Json
+})
+
+//Os ":" Significam que o que vem a segui é um parametro
+app.get('/produtos/:id', (req, res, next) =>{
+    res.send(bancoDeDados.getProduto(req.params.id)) //pega parametro da url
+})
+
+app.post('/produtos', (req, res, next) =>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
+app.put('/produtos/:id', (req, res, next) =>{
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
+
+app.delete('/produtos/:id', (req, res, next) =>{
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) //JSON
 })
 
 app.listen(porta, () =>{
