e4f56646bfc1e13da87ed3c1e45dbea27db660ca
Rogerio Damião <rogeriodominus@gmail.com>
2019-03-12 22:52:58 -0300

    Revião3 - 'Objetos'

---
+++ EsNext/revisao3.js
@@ -0,0 +1,26 @@
+// ES8: Object.values/Object.entries
+const obj = { a: 1,b: 2,c :3 }
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+
+// Melhorias na notação literal
+const nome = "Carla"
+const pessoa = {
+    nome,
+    ola(){
+        return`Olá me chamo ${nome}`
+    }
+}
+
+console.log(pessoa.ola())
+
+
+// Class
+class Animal{}
+class Cachorro extends Animal{
+    falar(){
+        return 'Au au!'
+    }
+}
+
